c27ee7f8360cf4953863505aea01ec2aba4d15b2
sancg <sanblackx1@gmail.com>
2023-01-18 18:13:10 -0500

    Simple node example

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const rectangle = require("./rectangle");
+var rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
 
-const { perimeter, area } = rectangle;
+function solveRectangle(l, b) {
+  console.log(`\x1b[0mSolving for Rectangle with L = ${l} + ${b}`);
 
-console.log(perimeter(2, 3));
+  if (l <= 0 || b <= 0) {
+    console.log(
+      `\x1b[31mRectangles dimensions should be greater than Zero: L = ${l}, B = ${b}`
+    );
+    return 0;
+  }
+
+  console.log(`\x1b[0mThe area of the rectangle is: ${rect.area(l, b)}`);
+  console.log(
+    `\x1b[0mThe perimeter of the rectangle is: ${rect.perimeter(l, b)}`
+  );
+}
+
+solveRectangle(4, 6);
+solveRectangle(0, 2);
+solveRectangle(10, 2);
+solveRectangle(-5, 5);
+// const rectangle = require("./rectangle");
+
+// const { perimeter, area } = rectangle;
+
+// console.log(perimeter(2, 3));
