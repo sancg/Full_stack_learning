b8703823542254797b5fbcfbbff81c53f2a3036e
sancg <sanblackx1@gmail.com>
2023-01-18 08:19:03 -0500

    Node modules example

---
+++ index.js
@@ -0,0 +1,5 @@
+const rectangle = require("./rectangle");
+
+const { perimeter, area } = rectangle;
+
+console.log(perimeter(2, 3));
---
+++ rectangle.js
@@ -0,0 +1,7 @@
+exports.perimeter = (x, y) => {
+  return 2 * (x + y);
+};
+
+exports.area = (x, y) => {
+  x * y;
+};
