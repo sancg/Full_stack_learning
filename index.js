5a1adb0bc25b8ad763ddda72dbda8626202a2eae
sancg <sanblackx1@gmail.com>
2023-01-18 18:15:03 -0500

    Exporting functions from Rectangle.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-};
+const rect = require("./rectangle");
 
 function solveRectangle(l, b) {
   console.log(`\x1b[0mSolving for Rectangle with L = ${l} + ${b}`);
