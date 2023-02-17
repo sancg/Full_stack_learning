aac39729c9f95a4da02eb262b53e6d87141c836a
sancg <sanblackx1@gmail.com>
2023-02-17 16:53:12 -0500

    :bulb: Learning: Use of FS, Path modules and how to served files

--- backend/node-http/index.js
+++ backend/node-http/index.js
@@ -1,26 +1,84 @@
 const http = require("http");
+const fs = require("fs"); //File system Core Module
+const path = require("path");
 
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
-
-  res.statusCode = 200;
-
-  res.setHeader("Content-Type", "text/html");
-  res.end(
-    `
-    <!DOCTYPE html>
-    <html>
-      <header>
-      </header>
-
-      <body>
-        <h1>Hello my gorgeous friend~</h1>
-      </body>
-    </html>`
+  console.log(
+    `\x1b[33mRequest to:\x1b[0m ${req.url}\n\x1b[33mBy method:\x1b[0m ${req.method}`
   );
+
+  if (req.method === "GET") {
+    let fileURL = "/";
+    if (req.url === fileURL) {
+      fileURL = "/index.html";
+    } else if (req.url === "/about") {
+      fileURL = "/about.html";
+    } else fileURL = req.url;
+
+    const filePath = path.resolve("./node-http/public" + fileURL);
+    const fileExt = path.extname(filePath);
+    console.log("Extension file:" + fileExt);
+
+    if (fileExt === ".html") {
+      fs.exists(filePath, (e) => {
+        if (!e) {
+          res.statusCode = 404;
+          res.setHeader("Content-type", "text/html");
+          res.end(`
+          <!DOCTYPE html>
+          <html>
+            <header>
+              <title>Error Page</title>
+            </header>
+      
+            <body>
+              <h1>Page not found</h1>
+            </body>
+          </html>
+          `);
+          return;
+        }
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else {
+      // If the file is different than HTML
+      res.statusCode = 404;
+      res.setHeader("Content-type", "text/html");
+      res.end(`
+      <!DOCTYPE html>
+      <html>
+        <header>
+          <title>Error Page</title>
+        </header>
+  
+        <body>
+          <h1>Page not found, and it is not an HTML file:${filePath}</h1>
+        </body>
+      </html>
+      `);
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-type", "text/html");
+    res.end(`
+      <!DOCTYPE html>
+      <html>
+        <header>
+          <title>Method = Not supported</title>
+        </header>
+  
+        <body>
+          <h1>Method not supported</h1>
+        </body>
+      </html>
+      `);
+  }
 });
 
 server.listen(port, hostname, () => {
