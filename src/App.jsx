134e299bf04a3b8d5283769fbff92527075ebb5a
Santiago Cano <sanblackx1@gmail.com>
2024-01-19 00:02:55 -0500

    Named vs. default exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+import Gallery, { Profile } from './Gallery';
+export default function App() {
   return (
-    <div className="App">
-      <h1>Simple frontEnd dev</h1>
-    </div>
+    <>
+      <Gallery />
+      <h2>From App</h2>
+      <Profile />
+    </>
   );
 }
-
-export default App;
